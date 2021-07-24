9f13648190eae4c4ea06749c8e819145bcb0d2be
danish sharma <danish45007@gmail.com>
2021-07-24 18:59:40 +0530

    nlp packages integrated

--- main.go
+++ main.go
@@ -2,12 +2,19 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
+	"os"
 
+	"github.com/Krognol/go-wolfram"
 	"github.com/joho/godotenv"
 	"github.com/shomali11/slacker"
+	"github.com/tidwall/gjson"
+	witai "github.com/wit-ai/wit-go"
 )
+
+var wolframClient *wolfram.Client
 
 func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
 	for event := range analyticsChannel {
@@ -22,16 +29,32 @@ func printCommandEvents(analyticsChannel <-chan *slacker.CommandEvent) {
 
 func main() {
 	godotenv.Load(".env")
-	slackBot := slacker.NewClient("xoxb-2323039060352-2305584906260-ooJ6dODDUm5oGLWMhrEJB8cW",
-		"xapp-1-A029608QS2V-2323047138656-b26fc207c5e54902f6188014f78e9351eb88f4bd6b05d696845d31f64c1832f0")
+	slackBot := slacker.NewClient(os.Getenv("SLACK_BOT_TOKEN"), os.Getenv("SLACK_APP_TOKEN"))
+	witClient := witai.NewClient(os.Getenv("WIT_AI_TOKEN"))
+	wolframClient := &wolfram.Client{AppID: os.Getenv("WOLFRAM_APP_TOKEN")}
 	go printCommandEvents(slackBot.CommandEvents())
-	slackBot.Command("query for bot -,<message>", &slacker.CommandDefinition{
+	slackBot.Command("query for bot - <message>", &slacker.CommandDefinition{
 		Description: "send any question to wolfram",
 		Example:     "what is the size of earth ?",
 		Handler: func(botCtx slacker.BotContext, request slacker.Request, response slacker.ResponseWriter) {
 			query := request.Param("message")
-			fmt.Println(query)
-			response.Reply("received")
+			witResponse, err := witClient.Parse(&witai.MessageRequest{
+				Query: query,
+			})
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(0)
+			}
+			data, _ := json.MarshalIndent(witResponse, "", "    ")
+			stringifyData := string(data[:])
+			targetData := gjson.Get(stringifyData, "entities.wolfram_search_query.0.value")
+			stringTargetData := targetData.String()
+			wolframResponse, err := wolframClient.GetSpokentAnswerQuery(stringTargetData, wolfram.Metric, 1000)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(0)
+			}
+			response.Reply(wolframResponse)
 		},
 	})
 
